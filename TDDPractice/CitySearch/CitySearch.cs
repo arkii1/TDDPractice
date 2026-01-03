5aef97e28e20c9111f51c73e2e0b2bd494568e56
arkii1 <tommy.mapp@hotmail.com>
2026-01-03 17:27:56 +0000

    Add CitySearch

---
+++ TDDPractice/CitySearch/CitySearch.cs
@@ -0,0 +1,46 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace TDDPractice.CitySearch
+{
+    public class CitySearch
+    {
+        string[] cities = new string[]
+        {
+            "Paris",
+            "Budapest",
+            "Skopje",
+            "Rotterdam",
+            "Valencia",
+            "Vancouver",
+            "Amsterdam",
+            "Vienna",
+            "Sydney",
+            "New York City",
+            "London",
+            "Bangkok",
+            "Hong Kong",
+            "Dubai",
+            "Rome",
+            "Istanbul"
+        };
+
+
+        public string[] Search(string input)
+        {
+            if (input == "*")
+                return cities;
+
+            if (input.Length >= 2)
+            {
+                return cities.Where(c => {
+                    if (c.Length < input.Length)
+                        return false;
+
+                    return c.ToLower().Contains(input.ToLower());
+                }).ToArray();
+            }
+            
+            return Array.Empty<string>();
+        }
+    }
+}
---
+++ TDDPractice/CitySearch/CitySearch_Tests.cs
@@ -0,0 +1,70 @@
+﻿namespace TDDPractice.CitySearch
+{
+    // From https://tddmanifesto.com/exercises/
+
+    [TestClass]
+    public class CitySearch_Tests
+    {
+        CitySearch citySearch;
+
+        [TestInitialize]
+        public void CitySearch_Initialize()
+        {
+            citySearch = new CitySearch();
+        }
+
+        [TestMethod]
+        public void CitySearch_Is_Not_Null()
+        {
+            Assert.IsNotNull(citySearch);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("a")]
+        public void CitySearch_Returns_Empty_On_Less_Than_Two_Characters(string input)
+        {
+            var expected = Array.Empty<string>();
+            CollectionAssert.AreEqual(expected, citySearch.Search(input));
+        }
+
+        [TestMethod]
+        [DataRow("Paris", new string[] {"Paris"} )]
+        [DataRow("Va", new string[] { "Valencia", "Vancouver" })]
+        [DataRow("ko", new string[] { "Skopje", "Bangkok", "Hong Kong" })]
+        [DataRow("*", new string[] 
+            {
+                "Paris",
+                "Budapest",
+                "Skopje",
+                "Rotterdam",
+                "Valencia",
+                "Vancouver",
+                "Amsterdam",
+                "Vienna",
+                "Sydney",
+                "New York City",
+                "London",
+                "Bangkok",
+                "Hong Kong",
+                "Dubai",
+                "Rome",
+                "Istanbul"
+            }
+        )]
+        public void CitySearch_Returns_Correct_Cities(string input, string[] expected)
+        {
+            CollectionAssert.AreEqual(expected, citySearch.Search(input));
+        }
+
+        [TestMethod]
+        [DataRow("paris", new string[] { "Paris" })]
+        [DataRow("va", new string[] { "Valencia", "Vancouver" })]
+        [DataRow("PARIS", new string[] { "Paris" })]
+        [DataRow("VA", new string[] { "Valencia", "Vancouver" })]
+        public void CitySearch_Is_Case_Insensitive(string input, string[] expected)
+        {
+            CollectionAssert.AreEqual(expected, citySearch.Search(input));
+        }
+    }
+}
