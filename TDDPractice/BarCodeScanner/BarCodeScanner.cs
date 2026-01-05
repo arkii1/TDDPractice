f91862e502f9d76a740758a193ea5780a6299092
arkii1 <tommy.mapp@hotmail.com>
2026-01-05 14:14:34 +0000

    Add point of sale (although was kind of a bad kata, would not recommend)

---
+++ TDDPractice/BarCodeScanner/BarCodeScanner.cs
@@ -0,0 +1,26 @@
+﻿namespace TDDPractice.BarCodeScanner
+{
+    public class BarCodeScanner
+    {
+        Dictionary<string, string> barcodes = new Dictionary<string, string>
+        {
+            { "12345", "$7.25" },
+            { "23456", "$12.50" }
+        };
+
+        public string Scan(string input)
+        {
+            if(String.IsNullOrEmpty(input))
+            {
+                return "Error: empty barcode";
+            }
+
+            if (!barcodes.ContainsKey(input))
+            {
+                return "Error: barcode not found";
+            }
+
+            return barcodes[input];
+        }
+    }
+}
---
+++ TDDPractice/BarCodeScanner/BarCodeScanner_Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace TDDPractice.BarCodeScanner
+{
+    [TestClass]
+    public class BarCodeScanner_Tests
+    {
+        BarCodeScanner scanner;
+
+        [TestInitialize]
+        public void BarCodeScanner_Initialize()
+        {
+            scanner = new BarCodeScanner();
+        }
+
+        [TestMethod]
+        public void BarCodeScanner_Is_Not_Null()
+        {
+            Assert.IsNotNull(scanner);
+        }
+
+        [TestMethod]
+        [DataRow("12345", "$7.25")]
+        [DataRow("23456", "$12.50")]
+        public void BarCodeScanner_Returns_Expected(string input, string expected)
+        {
+            var actual = scanner.Scan(input);
+        }
+
+        [TestMethod]
+        public void BarCodeScanner_Returns_Error_On_Not_Found()
+        {
+            var expected = "Error: barcode not found";
+            var actual = scanner.Scan("999");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BarCodeScanner_Returns_Empty_Barcode_On_Empty_Input()
+        {
+            var expected = "Error: empty barcode";
+            var actual = scanner.Scan("");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
